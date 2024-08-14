f43bdcf8f9a6c7e631639e29ff19e38d65f0097f
kchuang1 <kchuang1@nuvoton.com>
2024-08-14 15:38:28 +0800

    1. Update to fix Keil compiler 6 issue 	2. Fixed some warning in sample codes
    Change-Id: I25a14242aa55c59267b199aca2c1259ddf588ad2

--- SampleCode/StdDriver/FMC_CRC32/main.c
+++ SampleCode/StdDriver/FMC_CRC32/main.c
@@ -100,7 +100,7 @@ int32_t main(void)
         goto lexit;
     }
 
-    printf("\nLDROM (0x100000 ~ 0x%x) CRC32 checksum =>  ", (FMC_LDROM_BASE + FMC_MIN_LDROM_SIZE));
+    printf("\nLDROM (0x100000 ~ 0x%lx) CRC32 checksum =>  ", (FMC_LDROM_BASE + FMC_MIN_LDROM_SIZE));
 
     /* Erase the first page of LDROM */
     FMC_ENABLE_LD_UPDATE();
